d34c52e733b72fcfaf14d5dea8b147693aee60d2
עומר פוזזינסקי <omer_pojejinsky@wmrs-MBP.lan>
2024-11-26 18:57:05 +0200

    3

--- Algebra.java
+++ Algebra.java
@@ -9,7 +9,7 @@ public class Algebra {
 	    System.out.println(plus(2,3));   // 2 + 3
 	    System.out.println(minus(7,2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(-3,4));  // 3 * 4
+ 		System.out.println(times(3,4));  // 3 * 4
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		System.out.println(pow(5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
@@ -56,24 +56,15 @@ public class Algebra {
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {//לתקן
-		int original = x1;
-		if (x1 == 0 || x2 == 0) {
-			return 0;
-		}
-		if (x1 > 0 && x2 < 0 || x1 < 0 && x2 > 0) {
-			System.out.print("negative");
-			x1 = minus(x1, plus(x1, x1));
-			System.out.println(x1);
-			for(int i = 0;i < x2;i++){
-				x1 = minus(x1, original);
+		if (x1 > 0 && x2 > 0 || x1 < 0 && x2 < 0) {
+			int original = x1;
+			for(int i = 0;i < x2 - 1;i++) {
+				x1 = plus(x1,original);
 			}
+			return x1;
 		}
-		if (original > 0 && x2 > 0 || original < 0 && x2 < 0) {
-			for(int i = 0;i < x2;i++){
-				x1 = plus(x1, original);
-			}
-		}
-		return x1;
+
+		return -x1;
 	}
 
 	// Returns x^n (for n >= 0)
